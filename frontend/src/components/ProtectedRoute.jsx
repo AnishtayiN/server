561c12ceb28f1090b704bcced1a24ea6eca4e67f
AnishtayiN <massagetoanishtayin@gmail.com>
2026-08-07 10:19:29 +0330

    Create ProtectedRoute.jsx

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from 'react-router-dom'
+import { useStore } from '../store/useStore'
+
+export default function ProtectedRoute({ children }) {
+  const isAuthenticated = useStore((s) => s.isAuthenticated)
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />
+  }
+  return children
+}
